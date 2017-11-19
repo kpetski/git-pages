aa48b5081a45c3816e4f98a8ecdbdf5bffb12145
kpetski <keripetski@hotmail.com>
2017-11-18 18:17:15 -0600

    added tic-tac-toe component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TicTacToe from './components/TicTacToe'
 
 class App extends Component {
   render() {
@@ -8,11 +9,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Keri's Git Pages</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This React App contains information about the GitHub Pages I've published
         </p>
+        <TicTacToe />
       </div>
     );
   }
