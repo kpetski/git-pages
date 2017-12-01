f2cd73fc304fe48e6c49f2dcbc1287f67943a5af
kpetski <keripetski@hotmail.com>
2017-12-01 14:58:12 -0600

    usernames under wrong project

--- src/components/GitApiCards.js
+++ src/components/GitApiCards.js
@@ -8,6 +8,7 @@ class GitApiCards extends Component {
           <h1 style={{backgroundColor: "lightgray"}}>Git API Cards</h1>
           <p><strong>Takes an input of github user, and adds it to a list of github cards</strong></p>
           <p>React app working with components, state, and api call</p>
+          <p>Some Valid Username to try: jordwalke, sophiebits, zpao, azat-co, kpetski</p>
           <button onClick={()=> window.open("https://kpetski.github.io/git-api-cards/", '_blank')}>GitHub Page</button>
           <button className="button" onClick={()=> window.open("https://github.com/kpetski/git-api-cards", '_blank')}>GitHub Repo</button>     
       </div>
--- src/components/TableFetch.js
+++ src/components/TableFetch.js
@@ -8,7 +8,6 @@ class TableFetch extends Component {
           <h1 style={{backgroundColor: "lightgray"}}>Table Fetch</h1>
           <p><strong>Displays fetched data in a table</strong></p>
           <p>Simple React App to demonstrate fetching and displaying data.  Also utilizing bootstrap</p>
-          <p>Some Valid Username to try: jordwalke, sophiebits, zpao, azat-co, kpetski</p>
           <button onClick={()=> window.open("https://kpetski.github.io/table-fetch-example/", '_blank')}>GitHub Page</button>
           <button className="button" onClick={()=> window.open("https://github.com/kpetski/table-fetch-example", '_blank')}>GitHub Repo</button>     
       </div>
